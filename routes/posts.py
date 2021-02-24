09a1d85b9942a5c353baab0778f8919a80da424e
Matthew Mulford <mhmulford0@gmail.com>
2021-02-24 15:57:17 -0600

    can now delete posts

--- routes/posts.py
+++ routes/posts.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, g
 from models.Posts import Post
 from flask_expects_json import expects_json
 from models.db import db
+
 post_blueprint = Blueprint('posts', __name__)
 
 schema = {
@@ -38,12 +39,24 @@ def get_all_posts():
 
     return jsonify(all_posts), 200
 
-@post_blueprint.route('/<int:id>')
+@post_blueprint.route('/<int:id>', methods=['GET'])
 def get_one_post(id):
     one_post = {} 
     for post in Post.select().where(Post.id == id):
         one_post.update({'id': post.id, 'title': post.title, 'content': post.content})
     
     db.close()
-    
+
     return one_post
+
+@post_blueprint.route('/<int:id>', methods=['DELETE'])
+def delete_post(id):
+
+    try:
+        post = Post.get(Post.id == id)
+        post.delete_instance()
+        return {"msg": "post deleted"}, 200
+    except:
+        return {"error": "post does not exists"}, 400
+
+   
